3481f6d7d032229c0fdc13f9b8567b33d9be6391
Javier <javier.mediavilla.vegas@gmail.com>
2016-02-27 18:04:30 +0100

    Added the basic squeleton for the Minimax algorithm

--- src/Logic/Minimax.java
+++ src/Logic/Minimax.java
@@ -1,11 +1,61 @@
 package Logic;
 
 import lib.IGenerator;
+import lib.tree.ITreeVisitor;
+import lib.tree.Tree;
 
-public class Minimax {
+public class Minimax<T> {
 	public interface IMinimaxStructure<T> extends IGenerator<T>{
 		public int evaluate(T value);
 	}
 	
+	static class Evaluation implements ITreeVisitor{
+
+		@Override
+		public void visitLeaf(Tree leaf) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visitInternalNode(Tree internalNode) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void visitNode(Tree node) {
+			// TODO Auto-generated method stub
+		}
+		
+	}
 	
+	IMinimaxStructure<T> st;
+	Tree<T> tree;
+	int depth, breadth;
+	
+	public Minimax(IMinimaxStructure<T> st){
+		this.st = st;
+		depth = Integer.MAX_VALUE;
+		breadth = Integer.MAX_VALUE;
+	}
+	
+	public Minimax(IMinimaxStructure<T> st, int depth, int breadth){
+		this(st);
+		this.depth = depth;
+		this.breadth = breadth;
+	}
+	
+	public Tree<T> generate(T value){
+		tree = Tree.generateTree( value, st, depth, breadth );
+		return tree;
+	}
+	
+	public void setDepth(int depth){
+		this.depth = depth;
+	}
+	
+	public void setBreadth(int breadth){
+		this.breadth = breadth;
+	}
 }
